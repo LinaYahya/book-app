5282d0ad5df4c587cef717f27cb11a7332587ab7
Lina Ebeid <linaebe0@gmail.com>
2020-09-08 21:00:58 +0300

    add more/less book desc

--- components/BookDetails.js
+++ components/BookDetails.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Text, View, Image } from "react-native";
 import axios from "axios";
 
-export default function BookDetails({bookPre}) {
+export default function BookDetails({ bookPre }) {
   const [book, setBook] = useState();
+  const [textShown, setTextShown] = useState(false); //To show ur remaining Text
+  const [lengthMore, setLengthMore] = useState(false);
 
   const getBookInfo = async (bookURL) => {
     try {
@@ -18,23 +20,51 @@ export default function BookDetails({bookPre}) {
     getBookInfo(bookPre);
   }, [bookPre]);
 
+  const onTextLayout = useCallback((e) => {
+    setLengthMore(e.nativeEvent.lines.length >= 4); //to check the text is more than 4 lines or not
+  }, []);
+
   const renderStars = (no) => {
+    let stars = "";
     for (let i = no; i > 0; i--) {
-      return (<span>⭐</span>)
+      stars = stars + "⭐";
     }
+    return stars;
   };
+  
+  const toggleNumberOfLines = () => { //To toggle the show text or hide it
+    setTextShown(!textShown);
+}
   return (
     <View>
       {book && (
         <View>
-          <Image source={{ uri: book.imageLinks.thumbnail }}  style={{width: 200, height: 200}} />
+          <Image
+            source={{ uri: book.imageLinks.thumbnail }}
+            style={{ width: 200, height: 200 }}
+          />
           <Text>{book.title}</Text>
-          <Text>{book.description.replace(/<p>/gi, '')}</Text>
+          <Text
+            onTextLayout={onTextLayout}
+            numberOfLines={textShown ? undefined : 4}
+            style={{ lineHeight: 21 }}
+          >
+            {book.description.replace(/<p>/gi, "")}
+          </Text>
+          {lengthMore ? (
+            <Text
+              onPress={toggleNumberOfLines}
+              style={{ lineHeight: 21, marginTop: 10 }}
+            >
+              {textShown ? "Read less..." : "Read more..."}
+            </Text>
+          ) : null}
           <Text>page counts: {book.pageCount}</Text>
           <Text>by {book.authors?.map((e) => e + " ")}</Text>
           <View>
             <Text>
-            from {book.ratingsCount}
+              {renderStars(4)}
+              from {book.ratingsCount}
             </Text>
           </View>
         </View>
