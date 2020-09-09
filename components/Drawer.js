fbb429273356a68b38596ec731c0b5e67fa74b0d
Lina Ebeid <linaebe0@gmail.com>
2020-09-09 13:06:26 +0300

    format drawer by eslint

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,16 +1,15 @@
-import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Home';
 import DetailsScreen from '../screens/Search';
 
 const Drawer = createDrawerNavigator();
 
-
 export default function Menu() {
   return (
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Search" component={DetailsScreen} />
-      </Drawer.Navigator>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Search" component={DetailsScreen} />
+    </Drawer.Navigator>
   );
 }
