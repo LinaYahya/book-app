7b4153edcabe80f5ac64724cdf67d9502da493ea
Lina Ebeid <linaebe0@gmail.com>
2020-09-09 13:06:06 +0300

    format app by eslint

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Menu from './components/Drawer';
-
-
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Menu /> 
+      <Menu />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
