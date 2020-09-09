7d4fdeb0b5bc7909aeafa13fb73dbab53a39ec76
Lina Ebeid <linaebe0@gmail.com>
2020-09-09 13:06:55 +0300

    create search input at home comonent

--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,71 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState } from 'react';
+import {
+  TextInput, View, Button, Text, Platform, StatusBar, StyleSheet,
+} from 'react-native';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
-const Main = () => <View></View>
+const Main = () => <View />;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default function Home({ navigation }) {
+  const [textIn, setText] = useState();
+  const [books, setbooks] = useState();
+  const getBooks = async (bookname) => {
+    try {
+      const { data } = await axios(
+        `https://www.googleapis.com/books/v1/volumes?q=${bookname}`,
+      );
+      setbooks(data.items.slice(0, 2));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Main"
-        component={Main}
-        options={{
-          headerLeft: () => (
-            <Text onPress={() => navigation.openDrawer()}>Open</Text>
-          ),
-        }}
-      />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{
+            headerLeft: () => (
+              <Text onPress={() => navigation.openDrawer()}>Open</Text>
+            ),
+          }}
+        />
+      </Stack.Navigator>
+      <View>
+
+        <TextInput
+          style={{ height: 40, borderColor: 'black', borderWidth: 1 }}
+          onChangeText={(text) => setText(text)}
+          value={textIn}
+        />
+        <Button
+          onPress={() => {
+            getBooks(textIn);
+            navigation.navigate('Search', { books });
+          }}
+          title="Search"
+        />
+      </View>
+    </View>
   );
 }
+
+Home.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  navigation: PropTypes.object.isRequired,
+};
