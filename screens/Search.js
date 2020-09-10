a3075336e7543d52a573657ae9f2986c5a8ec9bb
Lina Ebeid <linaebe0@gmail.com>
2020-09-10 16:10:05 +0300

    wrap search screen by safeareaview

--- screens/Search.js
+++ screens/Search.js
@@ -1,10 +1,34 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import {
+  Platform, StatusBar, Text, Button, SafeAreaView,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import BooksSection from '../components/Book';
 
-export default function DetailsScreen() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default function DetailsScreen({ navigation, route }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Searchhhhhhhhhh</Text>
+      <BooksSection books={route.params.books} />
+      <Button title="Go back" onPress={() => navigation.goBack('Home')} />
+
+    </SafeAreaView>
   );
 }
+
+DetailsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  route: PropTypes.object.isRequired,
+};
