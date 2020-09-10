dae16c20a9b027925babedb29524d4d4ed52c7df
Lina Ebeid <linaebe0@gmail.com>
2020-09-10 16:04:37 +0300

    replace drawer navigation by stack screens

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Platform, StatusBar } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/Home';
+import SearchScreen from './screens/Search';
+import DetailsScreen from './screens/BookDetails';
 
-import Menu from './components/Drawer';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Menu />
+    <NavigationContainer style={{ marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Book Details" component={DetailsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
