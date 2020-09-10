d8ddbb4245f2373bbb8e3e5a8a038bcd4a12e68a
Lina Ebeid <linaebe0@gmail.com>
2020-09-10 17:39:51 +0300

    render search images by flatList component

--- components/Book.js
+++ components/Book.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import { Image, View, TouchableHighlight } from 'react-native';
+import {
+  Image, View, TouchableHighlight, FlatList,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import { useNavigation } from '@react-navigation/native';
 
-export default function BooksSection({ books }) {
+const BookImage = ({ id, selfLink, volumeInfo }) => {
   const navigation = useNavigation();
+
+  return (
+    <View key={id}>
+      <TouchableHighlight onPress={() => navigation.navigate('Book Details', { imageLink: selfLink })}>
+        <Image
+          source={{ uri: volumeInfo.imageLinks.thumbnail }}
+          style={{ width: 300, height: 300, margin: 25 }}
+        />
+      </TouchableHighlight>
+    </View>
+  );
+};
+
+export default function BooksSection({ books }) {
+  const renderItem = ({ item: { id, selfLink, volumeInfo } }) => (
+    <BookImage id={id} selfLink={selfLink} volumeInfo={volumeInfo} />
+  );
   return (
     <View>
-      {books?.map((book) => (
-        <View key={book.id}>
-          <TouchableHighlight onPress={() => navigation.navigate('Book Details', { imageLink: book.selfLink })}>
-            <Image
-              source={{ uri: book.volumeInfo.imageLinks.thumbnail }}
-              style={{ width: 100, height: 100 }}
-            />
-          </TouchableHighlight>
-        </View>
-      ))}
+      <FlatList
+        data={books}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+
     </View>
   );
 }
 
 BooksSection.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+BookImage.propTypes = {
+  id: PropTypes.string.isRequired,
+  selfLink: PropTypes.string.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
-  books: PropTypes.array.isRequired,
+  volumeInfo: PropTypes.object.isRequired,
 };
